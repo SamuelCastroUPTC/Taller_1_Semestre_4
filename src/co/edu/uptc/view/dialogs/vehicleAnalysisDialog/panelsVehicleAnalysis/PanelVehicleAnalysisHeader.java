d28c10d3af378ca54fb2cf1ded3d7b8cd204ec4c
Manuel.cruz <Mcruzchaparro01@gmail.com>
2024-09-11 10:31:40 -0500

    falta completar el readme

--- src/co/edu/uptc/view/dialogs/vehicleAnalysisDialog/panelsVehicleAnalysis/PanelVehicleAnalysisHeader.java
+++ src/co/edu/uptc/view/dialogs/vehicleAnalysisDialog/panelsVehicleAnalysis/PanelVehicleAnalysisHeader.java
@@ -1,12 +1,9 @@
 package co.edu.uptc.view.dialogs.vehicleAnalysisDialog.panelsVehicleAnalysis;
 
 import java.awt.Dimension;
-import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-
 import co.edu.uptc.globals.ConfigGlobalView;
 
 public class PanelVehicleAnalysisHeader extends JPanel {
@@ -32,13 +29,6 @@ public class PanelVehicleAnalysisHeader extends JPanel {
         JLabel title = new JLabel("Consultas De Cantidad De Vehículos");
         title.setFont(ConfigGlobalView.HEADER_FONT);
         title.setForeground(ConfigGlobalView.HEADER_TEXT_COLOR);
-        // GridBagConstraints gbc = new GridBagConstraints();
-        // gbc.gridx = 0;
-        // gbc.gridy = 0;
-        // gbc.weightx = 1.0;
-        // gbc.weighty = 1.0;
-        // gbc.anchor = GridBagConstraints.CENTER;
-        // gbc.fill = GridBagConstraints.NONE;
         this.add(title);
     }
 }
