44e510db529416b8092a7de7a45210aebbbb58b7
Samuel <sacapo2112@gmail.com>
2024-09-11 10:48:46 -0500

    se agrego intentar al readme

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 import co.edu.uptc.view.main.MainView;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         MainView mainView = new MainView();
         mainView.setVisible(true);
     }
